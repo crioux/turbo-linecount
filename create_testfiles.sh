61d3d34646eeec6545e2c081ca84cbe839c459a2
Christien Rioux <crioux@noctem.org>
2015-12-12 00:52:40 -0800

    test generator

--- create_testfiles.sh
+++ create_testfiles.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-dd if=/dev/urandom of=test1.txt bs=1000000 count=10
+scdd if=/dev/urandom of=test1.txt bs=1000000 count=10
 dd if=/dev/urandom of=test2.txt bs=1000000 count=100
 dd if=/dev/urandom of=test3.txt bs=1000000 count=1000
+cat test3.txt test3.txt test3.txt test3.txt test3.txt test3.txt test3.txt test3.txt test3.txt test3.txt > test4.txt
